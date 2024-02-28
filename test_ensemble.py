6018efb48db68147d53b6eaf28adecd5ec221c80
NhanBuiDuc <nhanbuiduc.work@gmail.com>
2024-02-28 18:41:04 +0700

    update

--- test_ensemble.py
+++ test_ensemble.py
@@ -174,23 +174,15 @@ def main():
             avrg_loss = epoch_total_loss / loader.dataset.__len__()
             _f1_score = f1_score(labels, predictions, average="macro")
 
-            print("%s loss=%0.4f AUC=%0.4f F1=%0.4f  Accuracy=%0.4f" %
-                  (avrg_loss, area_under_the_curve, _f1_score, accuracy))
+            accuracy = metrics.accuracy_score(labels, predictions)
             f.write("%s Epoch {} - loss={} AUC={} F1={} Accuracy={}\n".format(
                 avrg_loss, auc, _f1_score, accuracy))
-            accuracy = metrics.accuracy_score(labels, predictions)
-            f.write("Test Accuracy = {}\n".format(accuracy))
             print("Test Accuracy = %0.2f" % (accuracy))
             confusion = metrics.confusion_matrix(labels, predictions)
             f.write("Test Confusion Matrix = {}\n".format(confusion))
             print(confusion)
-            _f1_score = f1_score(labels, predictions, average="macro")
             f.write("Test F1 Score = {}\n".format(_f1_score))
-            print("Test F1 = %0.2f" % (_f1_score))
-            auc = sklearn.metrics.roc_auc_score(labels, predictions)
             f.write("Test AUC = {}\n".format(auc))
-            print("Test AUC = %0.2f" % (auc))
-
             f.flush()
         # Testing
         if run_test:
