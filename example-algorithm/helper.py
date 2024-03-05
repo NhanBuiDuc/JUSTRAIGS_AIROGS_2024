5fa648c2e2810276b723263e679ea1b07dc93942
NhanBuiDuc <nhanbuiduc.work@gmail.com>
2024-03-05 13:38:49 +0700

    update

--- example-algorithm/helper.py
+++ example-algorithm/helper.py
@@ -143,6 +143,10 @@ def write_referable_glaucoma_decision(result):
     with open(f"./output/multiple-referable-glaucoma-binary.json", "w") as f:
         f.write(json.dumps(result))
     print(result)
+    all_items = os.listdir("./")
+    # Print each item (file or folder)
+    for item in all_items:
+        print(item)
 
 
 def write_referable_glaucoma_decision_likelihood(result):
@@ -173,6 +177,10 @@ def write_referable_glaucoma_decision_likelihood(result):
     with open(f"./output/multiple-referable-glaucoma-likelihoods.json", "w") as f:
         f.write(json.dumps(result))
     print(result)
+    all_items = os.listdir("./")
+    # Print each item (file or folder)
+    for item in all_items:
+        print(item)
 
 
 def write_glaucomatous_features(result):
@@ -203,3 +211,7 @@ def write_glaucomatous_features(result):
     with open(f"./output/stacked-referable-glaucomatous-features.json", "w") as f:
         f.write(json.dumps(result))
     print(result)
+    all_items = os.listdir("./")
+    # Print each item (file or folder)
+    for item in all_items:
+        print(item)
