d201e19cab3d841bf3c17921fbffe84776db56f1
NhanBuiDuc <nhanbuiduc.work@gmail.com>
2024-03-13 14:02:01 +0700

    update

--- sobel.py
+++ sobel.py
@@ -3,8 +3,9 @@ import numpy as np
 
 # Read an image
 # Replace 'path_to_your_image.jpg' with the actual file path
-image = cv2.imread('path_to_your_image.jpg')
-
+image = cv2.imread('AIROGS_2024/images/TRAIN000000.JPG')
+# Resize the image to 500x500
+image = cv2.resize(image, (500, 500))
 # Convert the image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
